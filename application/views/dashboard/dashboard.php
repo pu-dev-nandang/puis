a6183c5e6a7a7fefc00ada85b811925e694a6969
Nandang <nndg.ace3@gmail.com>
2018-07-20 17:13:23 +0700

    attd

--- application/views/dashboard/dashboard.php
+++ application/views/dashboard/dashboard.php
@@ -1,9 +1,15 @@
-<h1>Dashboard</h1>
+<!--<h1>Dashboard</h1>-->
 
-<legend>Data Session</legend>
-<pre>
-    <?php print_r($this->session->all_userdata()); ?>
-</pre>
+<!--<legend>Data Session</legend>-->
+<!--<pre>-->
+<!--    --><?php //print_r($this->session->all_userdata()); ?>
+<!--</pre>-->
+
+<div class="col-md-8 col-md-offset-2 animated flipInX" style="text-align: center;margin-top: 50px;">
+    <img src="<?php echo base_url('images/logo-l.png'); ?>" style="max-width: 200px;">
+    <hr/>
+    <h4>Dashboard Under Construction</h4>
+</div>
 
 
 
--- application/views/page/academic/presensi/inputPresensi.php
+++ application/views/page/academic/presensi/inputPresensi.php
@@ -6,6 +6,10 @@
     #tableStdAttd thead th, #tableStdAttd tbody td {
         text-align: center;
     }
+    .btn-delete-attd {
+        padding: 1px 5px;
+        border-radius: 12px;
+    }
 </style>
 
 <div class="col-md-12">
@@ -15,7 +19,7 @@
                 <div class="row">
                     <div class="col-xs-4">
                         <select class="form-control" id="filterAttendanceType">
-                            <option selected disabled>--- Type ---</option>
+<!--                            <option selected disabled>--- Type ---</option>-->
                             <option value="0">Schedule</option>
                             <option value="1">UTS</option>
                             <option value="2">UAS</option>
@@ -40,9 +44,13 @@
 <script>
     $(document).ready(function () {
         window.totalMhs = 0;
+        getSelectOptionAttendance();
+        setTimeout(function () {
+            getDataAttendance();
+        },500);
     });
 
-    $('#filterAttendanceType').click(function () {
+    $('#filterAttendanceType').change(function () {
 
         var type = $('#filterAttendanceType').val();
 
@@ -63,7 +71,7 @@
     function getDataAttendance() {
         var type = $('#filterAttendanceType').val();
         var DataAttendance = $('#filterAttendance').val();
-        
+
         if(type==0 && DataAttendance!='' && DataAttendance!=null){
 
             var AttendanceID = DataAttendance.split('.')[0];
@@ -77,28 +85,26 @@
             var token = jwt_encode(data,'UAP)(*');
             $.post(url,{token:token},function (jsonResult) {
 
-                var htmlTable = '<table class="table table-bordered table-striped" id="dataTableSchedule">' +
+                var htmlTable = '<table class="table table-bordered" id="dataTableSchedule" style="margin-bottom: 70px !important;">' +
                     '        <thead>' +
                     '        <tr style="background-color: #436888;color: #ffffff;">' +
-                    '            <th rowspan="2"  style="width: 5%;">Attd</th>' +
+                    '            <th rowspan="2"  style="width: 1%;">Meet</th>' +
                     '            <th rowspan="2"  style="width: 5%;"><i class="fa fa-exchange"></i></th>' +
-                    '            <th colspan="2">Lecturer</th>' +
+                    '            <th rowspan="2" >Lecturer</th>' +
                     '            <th colspan="2">Students</th>' +
-                    '            <th rowspan="2" style="width: 10%;">Action</th>' +
-                    '            <th rowspan="2">BAP</th>' +
+                    '            <th rowspan="2" style="width: 5%;">Action</th>' +
+                    '            <th rowspan="2" style="width: 15%;">BAP</th>' +
+                    '            <th rowspan="2" style="width: 15%;">Note For Venue</th>' +
+                    '            <th rowspan="2" style="width: 15%;">Note For Equipment</th>' +
                     '        </tr>' +
                     '        <tr style="background-color: #436888;color: #ffffff;">' +
-                    '            <th style="width: 25%;">Name</th>' +
-                    '            <th style="width: 20%;">Date</th>' +
-                    // '            <th style="width: 5%;">In</th>' +
-                    // '            <th style="width: 5%;">Out</th>' +
-                    '            <th style="width: 5%;background: #438848;">P</th>' +
-                    '            <th style="width: 5%;background: #884343;">A</th>' +
+                    '            <th style="width: 3%;background: #438848;">P</th>' +
+                    '            <th style="width: 3%;background: #884343;">A</th>' +
                     '        </tr>' +
                     '        </thead>' +
                     '        <tbody id="dataRwSchedule">' +
                     '        </tbody>' +
-                    '    </table>';
+                    '    </table><hr/>';
 
                 $('#dataShow').html(htmlTable);
 
@@ -106,29 +112,9 @@
 
 
                 var tr = $('#dataRwSchedule');
+
                 for(var a=1;a<=14;a++){
-
-                    var btnAct = '<div class="btn-group">' +
-                        '  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
-                        '    <i class="fa fa-pencil-square-o" aria-hidden="true"></i> <span class="caret"></span>' +
-                        '  </button>' +
-                        '  <ul class="dropdown-menu">' +
-                        '    <li><a href="javascript:void(0);" class="inputLecturerAttd" data-no="'+a+'" data-id="'+dataAttd['ID']+'">Attendance Lecturer</a></li>' +
-                        '    <li><a href="javascript:void(0);" class="inputStudentAttd" data-no="'+a+'" data-id="'+dataAttd['ID']+'">Attendance Students</a></li>' +
-                        '    <li role="separator" class="divider"></li>' +
-                        '    <li><a href="javascript:void(0);" class="inputScheduleExchange" data-no="'+a+'" data-id="'+dataAttd['ID']+'">Set Schedule Exchange</a></li>' +
-                        '  </ul>' +
-                        '</div>';
-
-                    var btnDate = (dataAttd['Date'+a]!='' && dataAttd['Date'+a]!=null) ? moment(dataAttd['Date'+a]).format('dddd, DD MMM YYYY') : '-';
-
-                    var btnIn = (dataAttd['In'+a]!='' && dataAttd['In'+a]!=null) ? dataAttd['In'+a].substr(0,5) : '-';
-
-                    var btnOut = (dataAttd['Out'+a]!='' && dataAttd['Out'+a]!=null) ? dataAttd['Out'+a].substr(0,5) : '-';
-
-                    var BAP = (dataAttd['BAP'+a]!='' && dataAttd['BAP'+a]!=null) ? dataAttd['BAP'+a] : '-' ;
-
-                    var NameNip = (dataAttd['NIP'+a]!='' && dataAttd['NIP'+a]!=null) ? '<b>'+dataAttd['Name'+a]+'</b><br/>'+dataAttd['NIP'+a] : '-';
+                    var bg_td = (a%2==1) ? "background-color:#fafafa;" : "background-color:#ffffff;" ;
 
                     var Sc_Ex_Status = dataAttd['ScheduleExchange_Status'+a];
 
@@ -139,21 +125,63 @@
                         btn_Sc_Ex_Status = '<i class="fa fa-question-circle fa-2x" aria-hidden="true" style="color: blue;"></i>';
                     }
 
+                    var btnAct = '<div class="btn-group">' +
+                        '  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
+                        '    <i class="fa fa-pencil-square-o" aria-hidden="true"></i> <span class="caret"></span>' +
+                        '  </button>' +
+                        '  <ul class="dropdown-menu">' +
+                        '    <li><a href="javascript:void(0);" class="inputLecturerAttd" data-no="'+a+'" data-id="'+dataAttd['ID']+'">Input Attendance Lecturer</a></li>' +
+                        '    <li><a href="javascript:void(0);" class="inputStudentAttd" data-no="'+a+'" data-id="'+dataAttd['ID']+'">Input Attendance Students</a></li>' +
+                        '    <li role="separator" class="divider"></li>' +
+                        '    <li><a href="javascript:void(0);" class="inputScheduleExchange" data-no="'+a+'" data-id="'+dataAttd['ID']+'">Set Schedule Exchange</a></li>' +
+                        '  </ul>' +
+                        '</div>';
 
-                    tr.append('<tr>' +
-                        '            <td>'+a+'</td>' +
-                        '            <td>'+btn_Sc_Ex_Status+'</td>' +
-                        '            <td style="text-align: left;">'+NameNip+'</td>' +
-                        '            <td style="text-align: right;">' +
-                        '               <b>'+btnDate+'</b><br/>' +
-                        '           <b style="color: #438848;"><i class="fa fa-download" style="margin-right: 5px;" aria-hidden="true"></i> '+btnIn+' | </b>' +
-                        '           <b style="color: #436888;"><i class="fa fa-upload" style="margin-right: 5px;" aria-hidden="true"></i> '+btnOut+'</b>' +
-                        '           </td>' +
-                        '            <td>'+dataAttd['S_P'+a]+'</td>' +
-                        '            <td>'+dataAttd['S_A'+a]+'</td>' +
-                        '            <td>'+btnAct+'</td>' +
-                        '            <td>'+BAP+'</td>' +
-                        '        </tr>');
+                    var BAP = (dataAttd['BAP'+a]!='' && dataAttd['BAP'+a]!=null) ? dataAttd['BAP'+a] : '-' ;
+
+                    if(dataAttd['AttdLecturers'+a].length>0){
+                           var rw_span = (dataAttd['AttdLecturers'+a].length);
+                           tr.append('<tr style="'+bg_td+'">' +
+                               '            <td rowspan="'+rw_span+'">'+a+'</td>' +
+                               '            <td rowspan="'+rw_span+'">'+btn_Sc_Ex_Status+'</td>' +
+                               '            <td style="text-align: left;"><b>'+dataAttd['AttdLecturers'+a][0].Name+'</b>' +
+                               '                <span style="float: right;">'+moment(dataAttd['AttdLecturers'+a][0].Date).format('dddd, DD MMM YYYY')+' | '+dataAttd['AttdLecturers'+a][0].In.substr(0,5)+' - '+dataAttd['AttdLecturers'+a][0].Out.substr(0,5)+' | ' +
+                               '                <button class="btn btn-sm btn-default btn-default-danger btn-delete-attd" data-id="'+dataAttd['AttdLecturers'+a][0].ID+'"><i  class="fa fa-minus-circle"></i></button></span>'+
+                               '            </td>' +
+                               '            <td rowspan="'+rw_span+'">'+dataAttd['S_P'+a]+'</td>' +
+                               '            <td rowspan="'+rw_span+'">'+dataAttd['S_A'+a]+'</td>' +
+                               '            <td rowspan="'+rw_span+'">'+btnAct+'</td>' +
+                               '            <td rowspan="'+rw_span+'" style="text-align: left;">'+BAP+'</td>' +
+                               '            <td rowspan="'+rw_span+'" style="text-align: left;background: #ffc1072e;"></td>' +
+                               '            <td rowspan="'+rw_span+'" style="text-align: left;background: #8bc34a73;"></td>' +
+                               '        </tr>');
+
+                           for(var lecA=1;lecA<dataAttd['AttdLecturers'+a].length;lecA++){
+                               tr.append('<tr  style="'+bg_td+'">' +
+                                   '<td style="text-align: left;"><b>'+dataAttd['AttdLecturers'+a][lecA].Name+'</b>' +
+                                   '<span style="float: right;">'+moment(dataAttd['AttdLecturers'+a][lecA].Date).format('dddd, DD MMM YYYY')+' | '+dataAttd['AttdLecturers'+a][lecA].In.substr(0,5)+' - '+dataAttd['AttdLecturers'+a][lecA].Out.substr(0,5)+' | ' +
+                                   '<button class="btn btn-sm btn-default btn-default-danger btn-delete-attd" data-id="'+dataAttd['AttdLecturers'+a][0].ID+'"><i  class="fa fa-minus-circle"></i></button></span>'+
+                                   '</td>' +
+                                   '</tr>');
+                           }
+
+
+                    } else {
+                        tr.append('<tr  style="'+bg_td+'">' +
+                            '            <td>'+a+'</td>' +
+                            '            <td>'+btn_Sc_Ex_Status+'</td>' +
+                            '            <td style="text-align: left;">-</td>' +
+                            '            <td>'+dataAttd['S_P'+a]+'</td>' +
+                            '            <td>'+dataAttd['S_A'+a]+'</td>' +
+                            '            <td>'+btnAct+'</td>' +
+                            '            <td>'+BAP+'</td>' +
+                            '            <td rowspan="'+rw_span+'" style="text-align: left;background: #ffc1072e;"></td>' +
+                            '            <td rowspan="'+rw_span+'" style="text-align: left;background: #8bc34a73;"></td>' +
+                            '        </tr>');
+                    }
+
+
+
                 }
 
             });
@@ -175,8 +203,8 @@
         $.post(url,{token:token},function (jsonResult) {
 
             opt.empty();
-            opt.append('<option selected disabled>-- Select Schedule --</option>');
-            opt.append('<option disabled>----------------------</option>');
+            // opt.append('<option selected disabled>-- Select Schedule --</option>');
+            // opt.append('<option disabled>----------------------</option>');
             if(jsonResult.length>0){
 
                 for(var i=0;i<jsonResult.length;i++){
