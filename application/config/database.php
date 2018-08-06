8d2c71aa80ce00c8effde1fa55af19fc9e0fefa1
unknown <nandang.mulyadi@94-YPAP-IT-PC.pu.local>
2018-08-06 07:59:52 +0700

    db

--- application/config/database.php
+++ application/config/database.php
@@ -24,9 +24,13 @@ if($_SERVER['SERVER_NAME']!='localhost') {
 
 } else {
 
-    $db['default']['hostname'] = '192.168.0.19:3308';
-    $db['default']['username'] = 'dbadmin';
-    $db['default']['password'] = 'secret';
+    //$db['default']['hostname'] = '192.168.0.19:3308';
+    //$db['default']['username'] = 'dbadmin';
+    //$db['default']['password'] = 'secret';
+	
+	$db['default']['hostname'] = 'localhost';
+    $db['default']['username'] = 'root';
+    $db['default']['password'] = '';
     $db['default']['database'] = 'db_academic';
     $db['default']['dbdriver'] = 'mysqli';// support with MYSQl,POSTGRE SQL, ORACLE,SQL SERVER
     $db['default']['dbprefix'] = '';
