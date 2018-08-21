085a33f3ad430fa0974e47b58423bea806d479c1
unknown <nandang.mulyadi@94-YPAP-IT-PC.pu.local>
2018-08-21 08:23:41 +0700

    db library

--- application/controllers/auth/C_rekap.php
+++ application/controllers/auth/C_rekap.php
@@ -19,7 +19,7 @@ class C_rekap extends Globalclass {
         $this->server22 = $this->load->database('server22', TRUE);
     }
 
-    public function rekap_($db){
+    public function inportDBLibrary($db){
 
         $std = $this->db->query('SELECT s.NPM AS member_id, s.Name AS member_name, 
                                     s.DateOfBirth AS birth_date, s.Address AS member_address,
@@ -31,21 +31,25 @@ class C_rekap extends Globalclass {
         for($i=0;$i<count($std);$i++){
             $d = $std[$i];
 
-            $d['member_type_id'] = 2;
-            $d['gender'] = ($d['gender']=='P') ? 0 : 1;
-            $d['inst_name'] = 'Podomoro University';
-            $d['member_since_date'] = '2018-01-12';
-            $d['register_date'] = '2018-01-12';
-            $d['expire_date'] = '2018-01-12';
-            $d['input_date'] = '2018-08-20';
-            $d['last_update'] = '2018-08-20';
+//            $d['member_type_id'] = 2;
+//            $d['gender'] = ($d['gender']=='P') ? 0 : 1;
+//            $d['inst_name'] = 'Podomoro University';
+//            $d['member_since_date'] = '2018-01-12';
+//            $d['register_date'] = '2018-01-12';
+//            $d['expire_date'] = '2019-01-12';
+//            $d['input_date'] = '2018-08-20';
+//            $d['last_update'] = '2018-08-20';
 
-            $dataMem = $this->server22->select('member_id')->get_where('library.member', array('member_id'=>$d['member_id']))->result_array();
+            $this->server22->set('expire_date', '2019-01-12');
+            $this->server22->where('member_id', $d['member_id']);
+            $this->server22->update('library.member');
+
+//            $dataMem = $this->server22->select('member_id')->get_where('library.member', array('member_id'=>$d['member_id']))->result_array();
 
 
-            if(count($dataMem)<=0){
-                $this->server22->insert('library.member',$d);
-            }
+//            if(count($dataMem)<=0){
+//                $this->server22->insert('library.member',$d);
+//            }
 
 //            $std[$i] = $d;
 
@@ -62,7 +66,7 @@ class C_rekap extends Globalclass {
         return print_r(json_encode($data));
     }
 
-    public function rekap_2($db){
+    public function rekap_($db){
         $db_ = 'ta_'.$db;
         $dataStd = $this->db->query('SELECT * FROM '.$db_.'.students ORDER BY NPM ASC')->result_array();
 
