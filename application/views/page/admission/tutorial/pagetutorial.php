1f83b985bf2475820fe101bdf9acd1fe73dacf94
alhadi rahman <alhadi.rahman@pu.local>
2018-10-15 09:12:18 +0700

    update for tutorial page

---
+++ application/views/page/admission/tutorial/pagetutorial.php
@@ -0,0 +1,12 @@
+<div class="row">
+	<div class="col-md-12" align="center">
+		<h2>Tutorial Singkat</h2>
+	</div>
+</div>
+<div class="row">
+	<div class="col-md-12">
+		<div>
+		  <iframe src="<?php echo base_url('fileGetAny/admisi-Konfigurasi_Awal.pdf'); ?>" style="width:100%;height:700px;"></iframe>
+		</div>
+	</div>	
+</div>	
