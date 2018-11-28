23a3b85a9f228812398a81cb797ed4d2fbeb62a3
alhadi rahman <alhadi.rahman@pu.local>
2018-11-28 16:08:40 +0700

    .

--- application/views/page/finance/tagihan_mahasiswa/page_cek_tagihan_mhs.php
+++ application/views/page/finance/tagihan_mahasiswa/page_cek_tagihan_mhs.php
@@ -24,7 +24,7 @@
     <div class="col-xs-12" >
         <div class="panel panel-primary">
             <div class="panel-heading clearfix">
-                <h4 class="panel-title pull-left" style="padding-top: 7.5px;">Report</h4>
+                <h4 class="panel-title pull-left" style="padding-top: 7.5px;">Daftar Tagihan Mahasiswa</h4>
             </div>
             <div class="panel-body">
                 <div class="row">
