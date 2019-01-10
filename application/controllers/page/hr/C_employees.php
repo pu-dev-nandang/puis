1111eea4409abad3eac794d5c635dd1e92fda0c8
Nandang Mulyadi <pu-nandang@users.noreply.github.com>
2019-01-10 16:30:32 +0700

    Upload file lecuter

--- application/controllers/page/hr/C_employees.php
+++ application/controllers/page/hr/C_employees.php
@@ -34,6 +34,56 @@ class C_employees extends HR_Controler {
         $this->tab_menu($page);
     }
 
+    public function employees_files(){
+        $department = parent::__getDepartement();
+
+        $data['NIP'] = '';
+        $page = $this->load->view('page/'.$department.'/employees/employees_files',$data,true);
+        $this->tab_menu($page);
+    }
+
+    public function upload_files(){
+        $fileName = $this->input->get('fileName');
+        $Colom = $this->input->get('c');
+        $User = $this->input->get('u');
+
+
+        $config['upload_path']          = './uploads/files/';
+        $config['allowed_types']        = 'pdf';
+        $config['max_size']             = 8000; // 8 mb
+        $config['file_name']            = $fileName;
+
+        if(is_file('./uploads/files/'.$fileName)){
+            unlink('./uploads/files/'.$fileName);
+        }
+        $this->load->library('upload', $config);
+        if ( ! $this->upload->do_upload('userfile')){
+            $error = array('error' => $this->upload->display_errors());
+            return print_r(json_encode($error));
+        }
+        else {
+
+            $success = array('success' => $this->upload->data());
+            $success['success']['formGrade'] = 0;
+
+            // Cek apakah di db sudah ada
+            $dataNIP = $this->db->get_where('db_employees.files',array('NIP'=>$User))->result_array();
+            $dataUpdate = array(
+                $Colom => $fileName
+            );
+            if(count($dataNIP)>0){
+                $this->db->where('NIP', $User);
+                $this->db->update('db_employees.files',$dataUpdate);
+            } else {
+                $dataUpdate['NIP'] = $User;
+                $this->db->insert('db_employees.files',$dataUpdate);
+            }
+
+            return print_r(json_encode($success));
+        }
+
+    }
+
     public function input_employees(){
         $department = parent::__getDepartement();
         // get Prodi
