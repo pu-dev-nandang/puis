7f5960fff4365f14d5e4257b490b51368972d437
alhadi rahman <alhadi.rahman@podomorouniversity.ac.id>
2019-03-28 16:11:20 +0700

    Bug Fixed CRUD Master

--- application/controllers/api/C_rest.php
+++ application/controllers/api/C_rest.php
@@ -2267,7 +2267,7 @@ class C_rest extends CI_Controller {
                             $query=$this->db->query($sql, array($Departement,$Year))->result_array();
                             for ($i=0; $i < count($query); $i++) { 
                                     $this->m_budgeting->makeCanBeDelete('db_budgeting.cfg_set_post','CodePostBudget',$query[$i]['CodePostBudget']);
-                                }    
+                            }    
                             
                         }
                     }
