d48c8243394c386efba3803f736e537f2ca8461a
alhadi rahman <alhadi.rahman@podomorouniversity.ac.id>
2019-04-11 16:46:42 +0700

    Report Anggaran First

--- application/views/page/budgeting/finance/budget/report_anggaran_per_years.php
+++ application/views/page/budgeting/finance/budget/report_anggaran_per_years.php
@@ -7,8 +7,10 @@
 						<div class="form-group">
 							<label>Year</label>
 							<select class="select2-select-00 full-width-fix" id="Years">
-								
-							     <!-- <option></option> -->
+								 <?php for ($i=0; $i < count($arr_Year); $i++): ?>
+								 	<?php $selected = ($arr_Year[$i]['Activated'] == 1) ? 'selected' : ''; ?>
+								 	<option value="<?php echo $arr_Year[$i]['Year'] ?>" <?php echo $selected ?>><?php echo $arr_Year[$i]['Year'].'-'.($arr_Year[$i]['Year'] + 1) ?></option>
+								 <?php endfor ?>
 							 </select>
 						</div>	
 					</div>
@@ -17,14 +19,21 @@
 		</div>
 	</div>
 </div>
+
+<div class="row" style="margin-top: 10px;">
+	<div class="col-md-12">
+		
+	</div>
+</div>
 <script type="text/javascript">
+	var arr_post = [];
+	var arr_BudgetCategory = [];
+	var arr_HeadAccount = [];
 	$(document).ready(function() {
-		LoadFirstLoad();
+		$('#Years').select2({
+		   //allowClear: true
+		});
+
 		loadingEnd(500);
 	}); // exit document Function
-
-	function LoadFirstLoad()
-	{
-		 
-	}
 </script>
