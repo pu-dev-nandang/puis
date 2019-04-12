deeeae249f11d9b185a61af1c0e9f9ad7c505b34
Bismar Gazali <bismar.gazali@podomorouniversity.ac.id>
2019-04-12 16:44:32 +0700

    Revisi Version

--- application/config/routes.php
+++ application/config/routes.php
@@ -144,6 +144,8 @@ $route['human-resources/files_reviews'] = 'page/hr/c_employees/files_employees';
 
 // --- IT Version ---- ADD Bismar
 $route['it/version'] = 'page/it/c_it/version_data';
+$route['it/loadpageversion'] = 'page/it/c_it/loadpageversiondetail';
+
 
 
 //$route['database/lecturers'] = 'page/database/c_database/lecturers';
@@ -687,8 +689,9 @@ $route['api/__geteducationLevel'] = 'api/c_api/geteducationLevel';
 $route['api/__crudConfig'] = 'api/c_api/crudConfig';
 $route['api/__getdataversion'] = 'api/c_api/getlistversion';
 $route['api/__getdetailversion'] = 'api/c_api/getversiondetail';
+$route['api/__getdetailgroupmod'] = 'api/c_api/getgroupmoddetail';
 $route['api/__searchmodule'] = 'api/c_api/search_module';
-
+$route['api/__getdatagroupmodule'] = 'api/c_api/getlistgroupmodule';
 
 
 $route['api/__getMKByID'] = 'api/c_api/getMKByID';
@@ -697,6 +700,7 @@ $route['api/__getSemester'] = 'api/c_api/getSemester';
 $route['api/__getLecturer'] = 'api/c_api/getLecturer';
 $route['api/__getStudents'] = 'api/c_api/getStudents';
 $route['api/__getStudentsAdmission'] = 'api/c_api/getStudentsAdmission';
+$route['api/__getreqLecturer'] = 'api/c_api/getrequestLecturer';
 
 
 $route['api/__getAllMK'] = 'api/c_api/getAllMK';
@@ -911,9 +915,15 @@ $route['api/__getPosition'] = 'api/c_api/getPosition';
 $route['api/__getStatusEmployee'] = 'api/c_api/getStatusEmployee';
 $route['api/__getStatusVersion'] = 'api/c_api/getstatusversion';
 $route['api/__getStatusModule'] = 'api/c_api/getstatusmodule';
+$route['api/__getdivisiversion'] = 'api/c_api/getdivisiversion';
+$route['api/__dropdowngroupmod'] = 'api/c_api/dropdowngroupmodule';
+$route['api/__dropdownlistmodule'] = 'api/c_api/dropdownlistmodule';
+$route['api/__getpicversion'] = 'api/c_api/getversionpic';
+$route['api/__dropdowneditgroupmod'] = 'api/c_api/dropeditgroupmodule';
+$route['api/__dropeditmodule'] = 'api/c_api/dropeditmodule';
+
 
 $route['api/__crudKRSOnline'] = 'api/c_api/crudKRSOnline';
-
 $route['api/__crudCombinedClass'] = 'api/c_api/crudCombinedClass';
 $route['api/__getSimpleSearch'] = 'api/c_api/getSimpleSearch';
 $route['api/__getSimpleSearchStudents'] = 'api/c_api/getSimpleSearchStudents';
@@ -1091,6 +1101,10 @@ $route['vreservation/loadScheduleEquipment'] = 'page/vreservation/c_global/loadS
 
 // Request Document
 $route['requestdocument'] = 'page/request-document/c_requestdocument/list_requestdocument/';
+$route['api2/__getRequestdoc'] = 'api/c_api2/getrequestdocument';
+$route['api2/__getypedocument'] = 'api/c_api/getlistypedocument';
+$route['api2/__getmasrequestdoc'] = 'api/c_api2/getmasterrequestdoc';
+$route['api2/__crudrequestdoc'] = 'api/c_api2/crudrequestdocument';
 
 
 // test
@@ -1336,16 +1350,13 @@ $route['ApiServerToServer'] = 'c_login/ApiServerToServer';
 $route['importFormulirManual'] = 'page/admission/c_admission/importFormulirManual';
 $route['ImportupdateNoKwitansi'] = 'page/admission/c_admission/ImportupdateNoKwitansi';
 
-
 // for inject //
 $route['testadi'] = 'dashboard/c_dashboard/testadi';
 $route['testadi2'] = 'c_login/testadi2';
 
-
 // admin prodi
 $route['loginToAdminProdi'] = 'c_login/loginToAdminProdi';
 $route['loginToAdminFaculty'] = 'c_login/loginToAdminFaculty';
-
 
 // general affair
 $route['ga_schedule_exchange'] = 'page/ga/C_schedule_exchange/schedule_exchange_action';
