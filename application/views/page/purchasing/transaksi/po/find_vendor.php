4fd02baab12263d60862eb1248267f9c50942277
alhadi rahman <alhadi.rahman@podomorouniversity.ac.id>
2019-04-25 11:35:59 +0700

    PO First

---
+++ application/views/page/purchasing/transaksi/po/find_vendor.php
@@ -0,0 +1,17 @@
+<div class="row">
+	<div class="col-xs-3">
+		<div id = "page_pr_list"></div>
+	</div>
+	<div class="col-xs-6">
+		<div id = "page_pr_item_list"></div>
+	</div>
+	<div class="col-xs-3">
+		<div id = "page_pr_selected_list"></div>
+	</div>	
+</div>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+	    loadingEnd(1500);
+	}); // exit document Function
+</script>
