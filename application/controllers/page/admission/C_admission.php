6aab2adcea73946c282cdca3410f83cda4f55ba4
alhadi rahman <alhadi.rahman@podomorouniversity.ac.id>
2019-06-12 10:52:41 +0700

    Bug Fixed

--- application/controllers/page/admission/C_admission.php
+++ application/controllers/page/admission/C_admission.php
@@ -324,13 +324,33 @@ class C_admission extends Admission_Controler {
 
     public function formulir_offline_sale_save()
     {
+      $rs = array('msg'=> '','Status'=> 1);
       $input = $this->getInputToken();
       switch ($input['Action']) {
           case 'add':
-              $this->m_admission->inserData_formulir_offline_sale_save($input);
+              // check email already exist or not
+              $B_email = $this->m_admission->alreadyExistingEmail($input['email']);
+              if ($B_email) {
+                $this->m_admission->inserData_formulir_offline_sale_save($input);
+              }
+              else
+              {
+                $rs['Status'] = 0;
+                $rs['msg'] = 'Email already exist';
+              }
+              
               break;
           case 'edit':
-              $this->m_admission->editData_formulir_offline_sale_save($input);
+              $B_email = $this->m_admission->alreadyExistingEmail($input_arr['email']);
+              if ($B_email) {
+                $this->m_admission->editData_formulir_offline_sale_save($input);
+              }
+              else
+              {
+                $rs['Status'] = 0;
+                $rs['msg'] = 'Email already exist';
+              }
+              
               break;
           case 'delete':
               $query = $this->m_master->caribasedprimary('db_admission.sale_formulir_offline','ID',$input['CDID']);
@@ -340,6 +360,8 @@ class C_admission extends Admission_Controler {
               // print_r($FormulirCode);
               break;        
       }
+
+      echo json_encode($rs);
     }
 
     public function formulir_offline_salect_PIC()
