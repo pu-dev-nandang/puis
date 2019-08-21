3c067e3b527b4ee2624f2eb11e23eab998a2cf5c
Irfan Firdaus <irfan.firdaus@podomorouniversity.ac.id>
2019-08-21 11:08:13 +0700

    Update Connection DB Demo

--- application/config/database.php
+++ application/config/database.php
@@ -139,7 +139,7 @@ switch ($ServerName) {
 
         break;
     case 'demo.pcam.podomorouniversity.ac.id':
-        $db['default']['hostname'] = '10.1.30.102';
+        $db['default']['hostname'] = '10.1.30.59';
         $db['default']['username'] = 'db_itpu';
         $db['default']['password'] = 'Uap)(*&^%';
         $db['default']['database'] = 'db_academic';
@@ -155,7 +155,7 @@ switch ($ServerName) {
         $db['default']['autoinit'] = TRUE;
         $db['default']['stricton'] = FALSE;
 
-        $db['statistik']['hostname'] = '10.1.30.102';
+        $db['statistik']['hostname'] = '10.1.30.59';
         $db['statistik']['username'] = 'db_itpu';
         $db['statistik']['password'] = 'Uap)(*&^%';
         $db['statistik']['database'] = 'db_statistik';
