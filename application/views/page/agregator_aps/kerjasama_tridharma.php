364bbe708da07a1efc600825589f324c5d2cd16f
Nandang Mulyadi <pu-nandang@users.noreply.github.com>
2019-09-09 08:55:30 +0700

    View Prodi
    Signed-off-by: Nandang Mulyadi <pu-nandang@users.noreply.github.com>

--- application/views/page/agregator_aps/kerjasama_tridharma.php
+++ application/views/page/agregator_aps/kerjasama_tridharma.php
@@ -1,1 +1,41 @@
-<h1>kerjasama_tridharma</h1>
+<h1><small>kerjasama_tridharma</small> - Prodi : <span id="viewProdiID"></span> | <span id="viewProdiName"></span></h1>
+
+<script>
+
+    $(document).ready(function () {
+
+        var firstLoad = setInterval(function () {
+            var filterProdi = $('#filterProdi').val();
+
+            if(filterProdi!='' && filterProdi!=null){
+                loadPage();
+                clearInterval(firstLoad);
+            }
+
+        },1000);
+
+        setTimeout(function () {
+            clearInterval(firstLoad);
+        },5000);
+
+    });
+
+    $('#filterProdi').change(function () {
+        var filterProdi = $('#filterProdi').val();
+
+        if(filterProdi!='' && filterProdi!=null){
+            loadPage();
+        }
+
+    });
+
+    function loadPage() {
+        var filterProdi = $('#filterProdi').val();
+
+        if(filterProdi!='' && filterProdi!=null){
+            $('#viewProdiID').html(filterProdi);
+            $('#viewProdiName').html($('#filterProdi option:selected').text());
+        }
+    }
+
+</script>
