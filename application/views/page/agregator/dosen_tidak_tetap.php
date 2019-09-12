cee934143775d92dbe27ec1f9bf0c3c8b2dab052
Bismar Gazali <bismar.gazali@podomorouniversity.ac.id>
2019-09-12 16:09:06 +0700

    revisi total system agregator

--- application/views/page/agregator/dosen_tidak_tetap.php
+++ application/views/page/agregator/dosen_tidak_tetap.php
@@ -9,7 +9,7 @@
 <div class="well">
     <div class="row">
         <div class="col-md-12">
-            <div style="text-align: right"> <b>Download File : </b><button class="btn btn-success btn-circle" id="btndownloaadExcel" title="Dowload Excel"><i class="fa fa-file-excel-o"></i> </button></div>
+            <div style="text-align: right"><button class="btn btn-success" id="btndownloaadExcel" title="Dowload Excel"><i class="fa fa-file-excel-o margin-right"></i> Excel</button></div>
             <table class="table" id="dataTable">
                 <thead>
                 <tr>
@@ -46,14 +46,37 @@
 
             $('#listData').empty();
             if(jsonResult.length>0){
-
+                var AA = 0;
+                var L = 0;
+                var LK = 0;
+                var GB = 0;
+                var TP = 0;
+                var J = 0;
                 $.each(jsonResult,function (i,v) {
-
+                   
                     var td = '';
                     var total = 0;
                     $.each(v.details,function (i2, v2) {
+                        var det = v2.dataEmployees.length;
                         td = td+'<td>'+v2.dataEmployees.length+'</td>';
                         total = total + parseInt(v2.dataEmployees.length);
+                
+                        if(i2==0){
+                            TP = TP+det; //tenaga pengajar
+                        }
+                        else if(i2==1){ // Asisten Ahli
+                            AA = AA+det;
+                        }
+                        else if(i2==2){ // lektor
+                            L = L + det;
+                        }
+                        else if(i2==3){ // lektor kepala
+                            LK = LK+det;
+                        }
+                        else if(i2==4){ // Guru besar
+                            GB = GB+det;
+                        }
+                        
                     });
 
                     $('#listData').append('<tr>' +
@@ -61,8 +84,19 @@
                         '<td style="text-align: left;">'+v.Level+' - '+v.Description+'</td> '+td+
                         '<td style="text-align: center;background: lightyellow;border-left: 1px solid #ccc;">'+total+'</td>' +
                         '</tr>');
+                     J = J + total;
 
                 });
+
+                $('#listData').append('<tr>' +
+                    '<th colspan="2" class="tdJml">Jumlah</th>' +
+                    '<th class="tdJml">'+AA+'</th>' +
+                    '<th class="tdJml">'+L+'</th>' +
+                    '<th class="tdJml">'+LK+'</th>' +
+                    '<th class="tdJml">'+GB+'</th>' +
+                    '<th class="tdJml">'+TP+'</th>' +
+                    '<th class="tdJml">'+J+'</th>' +
+                    '</tr>');
 
                 passToExcel = jsonResult
             }
