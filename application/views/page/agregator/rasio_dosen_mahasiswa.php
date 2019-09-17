b1eee3d13b1f0f9b0d2b6397f3b5b16704a9ef5b
Nandang Mulyadi <pu-nandang@users.noreply.github.com>
2019-09-17 11:39:55 +0700

    rasio-dosen-mahasiswa
    Signed-off-by: Nandang Mulyadi <pu-nandang@users.noreply.github.com>

--- application/views/page/agregator/rasio_dosen_mahasiswa.php
+++ application/views/page/agregator/rasio_dosen_mahasiswa.php
@@ -4,6 +4,10 @@
     #tableData tr th, #tableData tr td {
         text-align: center;
     }
+
+    #tableLect tr th, #tableLect tr td {
+        text-align: center;
+    }
 </style>
 
 <div class="well">
@@ -41,7 +45,7 @@
 
             <p style="color: orangered;">
                 <br/>*) Mahasiswa yang terhitung adalah mahasiswa yang aktif (Tidak termasuk mahasiswa cuti / mangkir)
-                <br/>*) Dosen yang terhitung adalah dosen yang aktif (Sesuai status Lecturer HRD)
+                <br/>*) Dosen yang terhitung adalah semua dosen kontrak maupun tetap
             </p>
         </div>
     </div>
@@ -62,7 +66,7 @@
 
         setTimeout(function () {
             clearInterval(firstLoad);
-        },1000);
+        },5000);
 
 
     });
@@ -80,12 +84,17 @@
         var filterSemester = $('#filterSemester').val();
         if(filterSemester!='' && filterSemester!=null){
 
+            loading_modal_show();
+
             // console.log(filterSemester);
             var SemesterID = filterSemester.split('.')[0];
+            var Year = filterSemester.split('.')[1];
+
+            var SemesterText = $('#filterSemester option:selected').text();
 
             passToExcel = [];
 
-            var url = base_url_js+'api3/__getRasioDosenMahasiswa?smt='+SemesterID;
+            var url = base_url_js+'api3/__getRasioDosenMahasiswa?smt='+SemesterID+'&y='+Year;
             $.getJSON(url,function (jsonResult) {
 
                 $('#listData').empty();
@@ -97,17 +106,57 @@
                     var ds_x = 0;
 
                     $.each(jsonResult,function (i,v) {
+
+                        var toModal = {
+                            Prodi : v.Name,
+                            Desc : 'Dosen',
+                            Semester : SemesterText,
+                            Details : v.Lecturer_Sch_Fix
+                        };
+                        var tokenLect = jwt_encode(toModal,'UAP)(*');
+
+                        var viewLect = (v.Lecturer_Sch_Fix.length>0)
+                            ? '<a href="javascript:void(0);" class="showDetailLect" data-lec="'+tokenLect+'">'+v.Lecturer_Sch_Fix.length+'</a>'
+                            : v.Lecturer_Sch_Fix.length;
+
+
+                        var toModal_mhs = {
+                            Prodi : v.Name,
+                            Desc : 'Mahasiswa',
+                            Semester : SemesterText,
+                            Details : v.dataMahasiwa
+                        };
+                        var tokenMhs = jwt_encode(toModal_mhs,'UAP)(*');
+
+                        var viewMhs = (v.dataMahasiwa.length>0)
+                            ? '<a href="javascript:void(0);" class="showDetailMhs" data-mhs="'+tokenMhs+'">'+v.dataMahasiwa.length+'</a>'
+                            : v.dataMahasiwa.length;
+
+                        var toModal_mhs_2 = {
+                            Prodi : v.Name,
+                            Desc : 'Mahasiswa TA',
+                            Semester : SemesterText,
+                            Details : v.dataMahasiwaTA
+                        };
+                        var tokenMhs_2 = jwt_encode(toModal_mhs_2,'UAP)(*');
+
+                        var viewMhs_2 = (v.dataMahasiwaTA.length>0)
+                            ? '<a href="javascript:void(0);" class="showDetailMhs" data-mhs="'+tokenMhs_2+'">'+v.dataMahasiwaTA.length+'</a>'
+                            : v.dataMahasiwaTA.length;
+
+
+
                         $('#listData').append('<tr>' +
                             '<td style="border-right: 1px solid #ccc;">'+(i+1)+'</td>' +
                             '<td style="text-align: left;">'+v.Name+'</td>' +
-                            '<td style="border-left: 1px solid #ccc;">'+v.TotalLecturer+'</td>' +
-                            '<td style="border-left: 1px solid #ccc;">'+v.TotalMahasiwa+'</td>' +
-                            '<td style="border-left: 1px solid #ccc;">'+v.TotalMahasiwaTA+'</td>' +
+                            '<td style="border-left: 1px solid #ccc;">'+viewLect+'</td>' +
+                            '<td style="border-left: 1px solid #ccc;">'+viewMhs+'</td>' +
+                            '<td style="border-left: 1px solid #ccc;">'+viewMhs_2+'</td>' +
                             '</tr>');
 
-                        ds = ds + parseInt(v.TotalLecturer);
-                        ds_c = ds_c + parseInt(v.TotalMahasiwa);
-                        ds_x = ds_x + parseInt(v.TotalMahasiwaTA);
+                        ds = ds + parseInt(v.Lecturer_Sch_Fix.length);
+                        ds_c = ds_c + parseInt(v.dataMahasiwa.length);
+                        ds_x = ds_x + parseInt(v.dataMahasiwaTA.length);
                     });
 
                     $('#listData').append('<tr>' +
@@ -121,10 +170,104 @@
                     passToExcel = jsonResult;
                 }
 
+                loading_modal_hide();
+
             });
 
         }
 
     }
+
+    $(document).on('click','.showDetailLect',function () {
+        var  tokenLect = $(this).attr('data-lec');
+        var d = jwt_decode(tokenLect,'UAP)(*');
+
+
+
+        var tr = '';
+        if(d.Details.length>0){
+            $.each(d.Details,function (i,v) {
+
+                var NID = (v.NIDN!='' && v.NIDN!=null && v.NIDN!=0 && v.NIDN!='0') ? v.NIDN : '-';
+                NID = (v.NIDK!='' && v.NIDK!=null && v.NIDK!=0 && v.NIDK!='0') ? v.NIDK : NID;
+
+                var NUP = (v.NUP!='' && v.NUP!=null && v.NUP!=0 && v.NUP!='0') ? v.NUP : '-';
+
+                tr = tr+'<tr>' +
+                    '<td style="border-right: 1px solid #ccc;">'+(i+1)+'</td>' +
+                    '<td>'+NID+'</td>' +
+                    '<td>'+NUP+'</td>' +
+                    '<td style="text-align: left;">'+v.Name+'</td>' +
+                    '</tr>';
+            });
+        }
+
+
+        $('#GlobalModal .modal-header').html('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+            '<h4 class="modal-title">'+d.Prodi+' - '+d.Desc+' - '+d.Semester+'</h4>');
+        $('#GlobalModal .modal-body').html('<div class="row">' +
+            '    <div class="col-md-12">' +
+            '        <table class="table table-striped" id="tableLect" style="margin-bottom: 0px;">' +
+            '            <thead>' +
+            '            <tr>' +
+            '                <th style="width: 5%;">No</th>' +
+            '                <th style="width: 25%;">NIDN / NIDK</th>' +
+            '                <th style="width: 25%;">NUP</th>' +
+            '                <th>Name</th>' +
+            '            </tr>' +
+            '            </thead>' +
+            '            <tbody>'+tr+'</tbody>' +
+            '        </table>' +
+            '    </div>' +
+            '</div>');
+        $('#GlobalModal .modal-footer').html('<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>');
+        $('#GlobalModal').modal({
+            'show' : true,
+            'backdrop' : 'static'
+        });
+
+    });
+
+    $(document).on('click','.showDetailMhs',function () {
+        var  tokenMhs = $(this).attr('data-mhs');
+        var d = jwt_decode(tokenMhs,'UAP)(*');
+
+
+
+        var tr = '';
+        if(d.Details.length>0){
+            $.each(d.Details,function (i,v) {
+                tr = tr+'<tr>' +
+                    '<td style="border-right: 1px solid #ccc;">'+(i+1)+'</td>' +
+                    '<td>'+v.NPM+'</td>' +
+                    '<td style="text-align: left;">'+ucwords(v.Name)+'</td>' +
+                    '</tr>';
+            });
+        }
+
+
+        $('#GlobalModal .modal-header').html('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
+            '<h4 class="modal-title">'+d.Prodi+' - '+d.Desc+' - '+d.Semester+'</h4>');
+        $('#GlobalModal .modal-body').html('<div class="row">' +
+            '    <div class="col-md-12">' +
+            '        <table class="table table-striped" id="tableLect" style="margin-bottom: 0px;">' +
+            '            <thead>' +
+            '            <tr>' +
+            '                <th style="width: 5%;">No</th>' +
+            '                <th style="width: 25%;">NIM</th>' +
+            '                <th>Name</th>' +
+            '            </tr>' +
+            '            </thead>' +
+            '            <tbody>'+tr+'</tbody>' +
+            '        </table>' +
+            '    </div>' +
+            '</div>');
+        $('#GlobalModal .modal-footer').html('<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>');
+        $('#GlobalModal').modal({
+            'show' : true,
+            'backdrop' : 'static'
+        });
+
+    });
 
 </script>
