863646b978cce99258b75d98a74edfccccc9758a
alhadi rahman <alhadi.rahman@podomorouniversity.ac.id>
2020-07-09 11:34:37 +0700

    Scheduler APS table 1 Kerja Sama Done

--- application/controllers/api/C_scheduler.php
+++ application/controllers/api/C_scheduler.php
@@ -80,6 +80,8 @@ class C_scheduler extends CI_Controller {
           # code...
           break;
       }
+
+      echo json_encode($this->data);
     }
 
 
@@ -89,28 +91,87 @@ class C_scheduler extends CI_Controller {
 
     private function __s1(){
       // save to log
-      // $this->db->insert('aps_apt_rekap',[
-      //   'RunTime' => date('Y-m-d H:i:s'),
-      //   'TableName' => 's1'
-      // ]);
+      $this->db->insert('aps_apt_rekap.log',[
+        'RunTime' => date('Y-m-d H:i:s'),
+        'TableName' => 's1'
+      ]);
 
-      // $ID = $this->db->insert_id();
+      $ID = $this->db->insert_id();
 
       // get Prodi first
       $dataProdi = $this->getProdi();
-      $urlPost = base_url().'rest2/__get_data_kerja_sama_perguruan_tinggi';
-      $data_post = [
-        'token' => 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdXRoIjoiczNDcjNULUc0TiIsIm1vZGUiOiJEYXRhS2VyamFTYW1hQWdncmVnYXRvciIsIlByb2RpSUQiOiI0In0.91PdePvZuzA4NcnbbGubucQB-OVOyrnMEhfb-vNXWOQ',
-        'start' => 0,
-        'length' => 10000,
-        'search[value]' => '',
-        'search[regex]' => false,
-        'draw' => 1,
+      $param = [
+        'auth' => 's3Cr3T-G4N',
+        'mode' => 'DataKerjaSamaAggregator',
       ];
-      $postTicket = $this->m_master->postApiPHP($urlPost,$data_post);
-      print_r($postTicket);
+      $urlPost = base_url().'rest2/__get_data_kerja_sama_perguruan_tinggi';
+      $Year = date('Y');
+      $Month = date('m');
+      $DateCreated = $Year.'-'.$Month.'-'.date('d');
+      for ($i=0; $i < count($dataProdi); $i++) { 
+        $ProdiID = $dataProdi[$i]['ID'];
+        $param['ProdiID'] = $ProdiID;
+        $token = $this->jwt->encode($param,"UAP)(*");
+        $data_post = [
+          'token' => $token,
+          'start' => 0,
+          'length' => 10000,
+          'search[value]' => '',
+          'search[regex]' => false,
+          'draw' => 1,
+        ];
+        try {
+          // remove old data first by years and month
+          $this->db->query(
+            'delete from aps_apt_rekap.s1 where Year(DateCreated) = "'.$Year.'" and Month(DateCreated) = "'.$Month.'" and ProdiID = '.$ProdiID
+          );
 
+          $postTicket = $this->m_master->postApiPHP($urlPost,$data_post);
+          $result = (array) json_decode($postTicket,true);
+          $getData =  $result['data'];
+          for ($j=0; $j < count($getData); $j++) { 
+            $row = $getData[$j];
+            $Tingkat = 'Internasional';
+            if ($row[3] == 1) {
+              $Tingkat = 'Nasional';
+            }
 
+            if ($row[4] == 1) {
+              $Tingkat = 'Lokal';
+            }
+
+            $WaktuDurasi = 'Start : '.$row[12]."\n".'End : '.$row[8]."\n".$row[16].' days';
+            $file =  (array) json_decode($row[7],true);
+            $BuktiKerjasama =base_url().'fileGetAny/cooperation-'.$file[0];
+            $dataSave = [
+              'ProdiID' => $ProdiID,
+              'Lembaga' => $row[1],
+              'Kategory' =>   $row[13],
+              'Tingkat' =>   $Tingkat,
+              'JudulKegiatan' =>   $row[10],
+              'Manfaat' =>   $row[11],
+              'WaktuDurasi' =>   $WaktuDurasi ,
+              'BuktiKerjasama' =>   $BuktiKerjasama,
+              'MasaBerlaku' =>   $row[8],
+              'Semester' =>   $row[9],
+              'DateCreated' =>   $DateCreated,
+            ];
+
+            $this->db->insert('aps_apt_rekap.s1',$dataSave);
+
+          }
+          
+        } catch (Exception $e) {
+           print_r($e);
+        }
+
+      }
+
+      $this->data['status'] = 1;
+      if ($this->data['status'] == 1) {
+        $this->db->where('ID',$ID);
+        $this->db->update('aps_apt_rekap.log',['Status' => 1]);
+      }
     }
 
 }
